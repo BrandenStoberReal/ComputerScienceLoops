9e10183d1690ad266f10046190e9c91d3ebe38ef
Branden <theeladdergoat@gmail.com>
2022-01-31 12:58:38 -0500

    Finished task

--- ComputerScienceLoops/ComputerScienceLoops.py
+++ ComputerScienceLoops/ComputerScienceLoops.py
@@ -1,1 +1,46 @@
+"""
+More Loops
+Exercise 4.4 A
+"""
+import os
+import math
+import random
 
+"""
+Success = 0
+while (Success == 0):
+    Num = input("Please enter a number greater then 1 and below 100: ")
+    if (int(Num) < 1 or int(Num) > 100):
+        print("That is an invalid number! Try Again!")
+    else:
+        print("Thank you for your correct number!")
+        Success = 1
+"""
+
+"""
+FavoriteColor = "Blue"
+Success = 0
+Attempts = 0
+
+while (Success == 0):
+    Color = input("Whats your favorite color?: ")
+    if (Color.lower() != FavoriteColor.lower()):
+        print("That is a bad color! Please try again!")
+        Attempts  = Attempts + 1
+    else:
+        print("Now thats a good color! It took you " + str(Attempts) + " tries to pick a nice color!")
+        Success = 1
+"""
+
+"""
+Sum = 0
+Numbers = int(input("How many numbers would you like to add today?: "))
+Increment = 0
+
+for _ in range(Numbers):
+    Increment = Increment + 1
+    Num = int(input("Number to add #" + str(Increment) + ":"))
+    Sum = Sum + Num
+
+print("Your total sum is " + str(Sum) + "!")
+"""
